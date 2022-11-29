db78f9115a6cde869540c9ab539b43257ba48294
redman13 <74981904+RedMan13@users.noreply.github.com>
2022-11-29 04:15:09 +0000

    stuff with info

--- commands/info.js
+++ commands/info.js
@@ -9,6 +9,6 @@ module.exports = {
 because i remade the bot and so am transfering the commands over from the old code to the modern system.
 
 can i help?
-yes actually, the repo for the bot is (RedMan13/arcade-tools)[https://github.com/RedMan13/arcade-tools] and the bot is hosted on (Replit)[https://replit.com/@RedMan13/arcade-tools], the orignal code and databases is in the folder "old".`)
+yes actually, the repo for the bot is <https://github.com/RedMan13/arcade-tools> and the bot is hosted on <https://replit.com/@RedMan13/arcade-tools>, just ask for access, the orignal code and databases is in the folder "old".`)
     },
 };
