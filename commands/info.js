56b08fe592e4644a5b211069525e7720e9d77cb3
godslayerakp <74981904+RedMan13@users.noreply.github.com>
2023-06-11 02:56:31 -0700

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -5,10 +5,12 @@ module.exports = {
     lDesc: 'just run it olo',
     args: [],
     execute: async (message) => {
-        message.channel.send(`Q: why are there so few commands? 
-A: because i remade the bot and so am transfering the commands over from the old code to the modern system.
+        message.channel.send(`### Q: why are there so few commands? 
+A: because i remade the bot and so am transfering the commands over from the old code to the modern 
+     system.
 
-Q: can i help?
-A: yes actually, the repo for the bot is <https://github.com/RedMan13/arcade-tools> and the bot is hosted on <https://replit.com/@RedMan13/arcade-tools>, just ask for access, the orignal code and databases is in the folder "old".`)
+### Q: can i help?
+A: yes actually, the repo for the bot is [on here](<https://github.com/RedMan13/arcade-tools>), the orignal code 
+     and databases is in the folder "old".`)
     },
 };
